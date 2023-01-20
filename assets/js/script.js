87004e4e9da57501df34a81d2ace9e0ad7854b5c
Mariah Wear <wearmariahk@gmail.com>
2023-01-20 12:26:16 -0700

    finalizing file structure, adding some HTML framework

---
+++ assets/js/script.js
@@ -0,0 +1,2 @@
+/*To get the icon for the weather, use this URL as the src attribute in an <img> tag: (look for the icon property and use this:
+var iconUrl = `https://openweathermap.org/img/w/${**your-data-ojb**.icon}.png`;*/
